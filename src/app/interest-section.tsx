9883e4f0dc2f7d90017c808b937ad282eedb4df8
Tim Chen <Tim.chen@maibornwolff.de>
2024-03-21 23:44:39 +0100

    refactor interests into non sequential fade

--- src/app/interest-section.tsx
+++ src/app/interest-section.tsx
@@ -12,33 +12,44 @@ const interestsMap = {
   
 
 export default function InterestSection() {
-    const [isVisible, setIsVisible] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
         if (entry.isIntersecting) {
           setIsVisible(true);
-          observer.unobserve(entry.target); 
+          observer.unobserve(entry.target);
         }
       });
-    }, {
-      threshold: 0.1 
     });
 
     if (ref.current) {
       observer.observe(ref.current);
     }
 
-    return () => observer.disconnect();
+    return () => {
+      observer.disconnect();
+    };
   }, []);
     return (
-    <div ref={ref} className="flex justify-center flex-wrap gap-14">
-      {Object.entries(interestsMap).map(([name, image], index) => (
-        <Interest key={name} name={name} image={image} isVisible={isVisible} 
-        animationDelay={index * 100} />
-      ))}
-    </div>
-  );
-}
+        <div ref={ref} className={`flex justify-center flex-wrap gap-14 transition-all ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'} transition duration-700 ease-out`}>
+            <Interest name="Frontend" image="/interests/React-icon.svg" />
+            <Interest name="Backend" image="/interests/Spring_Boot.svg.png" />
+            <Interest
+              name="DevOps"
+              image="/interests/Kubernetes-icon-color.svg"
+            />
+            <Interest name="Consumer tech" image="/interests/gpu.png" />
+            <Interest
+              name="Extended reality"
+              image="/interests/vr-glasses.png"
+            />
+            <Interest
+              name="Artificial intelligence"
+              image="/interests/ai.png"
+            />
+          </div>
+    )
+  }
